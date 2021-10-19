a7e9b055e24ca7acb7eeb64853d5bb1f11e580af
Andy O'Bryan <andrew.obryan@gmail.com>
2021-10-18 20:14:03 -0400

    added coloring for icons

--- Project3/static/js/logic.js
+++ Project3/static/js/logic.js
@@ -19,7 +19,7 @@ document.getElementById('fit').addEventListener('click', () => {
   map.addControl(new mapboxgl.FullscreenControl());
 
 //FDA URL
-var limit = 50;
+var limit = 100;
 var url = `https://api.fda.gov/food/enforcement.json?limit=${limit}`;
 
 // Grab the data with d3
@@ -43,19 +43,28 @@ if (state !== "PR") {
   var postal_code = fdaresponse.results[i].postal_code.substring(0, 5);
   var voluntary_mandated = fdaresponse.results[i].voluntary_mandated;
   var fdaclass = fdaresponse.results[i].classification;
+  if (fdaclass == "Class I"){
+    var color_ = "#FBFA02"
+  } else if (fdaclass == "Class II") {
+    var color_ = "#FCC405"
+  } else {
+    var color_ = "#FC0505"
+  }
+  console.log(color_)
   var notificationtype =  fdaresponse.results[i].initial_firm_notification;
   // This is the secondary API URL to pull the postal code
   var zipurl_us = `https://api.zippopotam.us/us/${postal_code}`;
-
   // Starting the second D3 Json section with the ZIP results
   const zipresponse = await d3.json(zipurl_us)
 
   // Need lat and long to pull the marker coordinates
   var zip_lat = zipresponse.places[0].latitude;
   var zip_long = zipresponse.places[0].longitude;
-
+  
   // Set the marker for reach of the coordinates with the pop-up that provides clarifying information
-  var marker = new mapboxgl.Marker().setLngLat([zip_long, zip_lat])
+  var marker = new mapboxgl.Marker({
+    color: color_,
+    }).setLngLat([zip_long, zip_lat])
     .setPopup(
       new mapboxgl.Popup({ offset: 25 }) // add popups
         .setHTML(`<h3>${firm}</h3><hr><p>Recall Description: ${reason_for_recall}</p><hr><p>${voluntary_mandated}</p><hr><p>${fdaclass}</p>`))
@@ -71,44 +80,7 @@ const legend = document.getElementById('legend')
 }
 });
 
-}, false);
-
-// JQuery to bring in pagination
-$(document).ready(function() {
-  var totalRows = $('#tblData').find('tbody tr:has(td)').length;
-  console.log(totalRows);
-  var recordPerPage = 10;
-  var totalPages = Math.ceil(totalRows / recordPerPage);
-  console.log(`total # of pages${totalPages}`);
-  var $pages = $('<div id="pages"><p>Pages: </p></div>');
-  for (i = 0; i < totalPages; i++) {
-    $('<span class="pageNumber">' + (i + 1) + '</span>').appendTo($pages);
-  }
-  $pages.appendTo('#tblData');
-
-  $('.pageNumber').hover(
-    function() {
-      $(this).addClass('focus');
-    },
-    function() {
-      $(this).removeClass('focus');
-    }
-  );
-
-  $('table').find('tbody tr:has(td)').hide();
-  var tr = $('table tbody tr:has(td)');
-  for (var i = 0; i <= recordPerPage - 1; i++) {
-    $(tr[i]).show();
-  }
-  $('span').click(function(event) {
-    $('#tblData').find('tbody tr:has(td)').hide();
-    var nBegin = ($(this).text() - 1) * recordPerPage;
-    var nEnd = $(this).text() * recordPerPage - 1;
-    for (var i = nBegin; i <= nEnd; i++) {
-      $(tr[i]).show();
-    }
-  });
-});
+}, false);  
 
 // added for potential search
 
@@ -149,6 +121,45 @@ $(document).ready(function() {
   });
 
 })(document);
+
+// JQuery to bring in pagination
+$(document).ready(function() {
+  var totalRows = $('#tblData').find('tbody tr:has(td)').length;
+  console.log(totalRows);
+  var recordPerPage = 10;
+  var totalPages = Math.ceil(totalRows / recordPerPage);
+  console.log(`total # of pages${totalPages}`);
+  var $pages = $('<div id="pages"><p>Pages: </p></div>');
+  for (i = 0; i < totalPages; i++) {
+    $('<span class="pageNumber">' + (i + 1) + '</span>').appendTo($pages);
+  }
+  $pages.appendTo('#tblData');
+
+  $('.pageNumber').hover(
+    function() {
+      $(this).addClass('focus');
+    },
+    function() {
+      $(this).removeClass('focus');
+    }
+  );
+
+  $('table').find('tbody tr:has(td)').hide();
+  var tr = $('table tbody tr:has(td)');
+  for (var i = 0; i <= recordPerPage - 1; i++) {
+    $(tr[i]).show();
+  }
+  $('span').click(function(event) {
+    $('#tblData').find('tbody tr:has(td)').hide();
+    var nBegin = ($(this).text() - 1) * recordPerPage;
+    var nEnd = $(this).text() * recordPerPage - 1;
+    for (var i = nBegin; i <= nEnd; i++) {
+      $(tr[i]).show();
+    }
+  });
+});
+
+
 
 
 // Paste in from AM for circle chart thing
